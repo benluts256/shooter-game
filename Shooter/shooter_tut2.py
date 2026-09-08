2b118978ab68fc295167d3f0db3a7c7f54628702
lutaaya benon <lutaayabenon4@gmail.com>
2026-09-08 15:37:39 +0300

    update file location

--- Shooter/shooter_tut2.py
+++ Shooter/shooter_tut2.py
@@ -33,7 +33,7 @@ class Soldier(pygame.sprite.Sprite):
 		self.speed = speed
 		self.direction = 1
 		self.flip = False
-		img = pygame.image.load(f'img/{self.char_type}/Idle/0.png')
+		img = pygame.image.load(f'Shooter/img/{self.char_type}/Idle/0.png')
 		self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
 		self.rect = self.image.get_rect()
 		self.rect.center = (x, y)
